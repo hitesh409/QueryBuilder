c7d008d13ddebcbb615b0934f1366db77ca3a489
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2024-03-06 10:12:00 +0530

    Registering Connection string in Program.cs

--- BackendAPI/Program.cs
+++ BackendAPI/Program.cs
@@ -1,6 +1,14 @@
+using BackendAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews();
+var provider = builder.Services.BuildServiceProvider();
+var config = provider.GetRequiredService<IConfiguration>();
+builder.Services.AddDbContext<QueryBuilderContext>(item => item.UseSqlServer(config.GetConnectionString("con")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
