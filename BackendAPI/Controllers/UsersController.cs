47a71cb354e5f49e433c3c21a0e9b21353a02703
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2024-03-07 09:10:28 +0530

    UserController: Login using JWT auth

--- BackendAPI/Controllers/UsersController.cs
+++ BackendAPI/Controllers/UsersController.cs
@@ -1,7 +1,14 @@
 ﻿using BackendAPI.Models;
+using BCrypt.Net;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace BackendAPI.Controllers
 {
@@ -9,11 +16,13 @@ namespace BackendAPI.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        private readonly QueryBuilderContext context;
+        private readonly QueryBuilderContext _context;
+        private readonly IConfiguration _config;
 
-        public UsersController(QueryBuilderContext context)
+        public UsersController(QueryBuilderContext context,IConfiguration config)
         {
-            this.context = context;
+            this._context = context;
+            this._config = config;
         }
 
         [HttpPost("register")]
@@ -36,9 +45,46 @@ namespace BackendAPI.Controllers
 
             user.PasswordHash = passwordHash;
 
-            await context.Users.AddAsync(user);
-            await context.SaveChangesAsync();
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
             return Ok(user);
+        }
+
+        [HttpPost("login")]
+        
+        public async Task<IActionResult> LoginUser([FromBody] LoginModel login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values.SelectMany(e=>e.Errors).Select(e=>e.ErrorMessage).ToArray());
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(u=>u.Username == login.Username);
+            if(user == null)
+            {
+                return Unauthorized("Invalid credentials");
+            }
+
+            var isPasswordValid = BCrypt.Net.BCrypt.Verify(login.Password,user.PasswordHash);
+
+            if (!isPasswordValid)
+            {
+                return Unauthorized("Invalid credential");
+            }
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var Sectoken = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            var token = new JwtSecurityTokenHandler().WriteToken(Sectoken);
+
+            return Ok(token);
+
         }
     }
 }
@@ -48,4 +94,13 @@ public class RegisterModel
     public string UserName { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
+}
+
+public class LoginModel
+{
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    public string Password { get; set; }
 }
