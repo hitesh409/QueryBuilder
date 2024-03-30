8ac8138de990d6d78cefb639981f446d90a796e0
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2024-03-30 09:36:58 +0530

    auth service for authentication using JWT

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/auth.guard';
+import { SharedComponent } from './shared/shared.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'shared', component: SharedComponent ,canActivate:[AuthGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
