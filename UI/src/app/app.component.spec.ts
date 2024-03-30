786f99325bafec6575d9b53338b955cd01d20ed5
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2024-03-30 09:08:45 +0530

    initial committ

--- UI/src/app/app.component.spec.ts
+++ UI/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'QueryBuilderUI'`, () => {
+  it(`should have as title 'UI'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('QueryBuilderUI');
+    expect(app.title).toEqual('UI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, QueryBuilderUI');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, UI');
   });
 });
