1746209f61f58ea1ae93c06d5fc7aea81dfdd382
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2024-03-31 10:02:13 +0530

    auth-services and guard services

---
+++ UI/src/app/services/auth-interceptor-service/auth-interceptor.service.ts
@@ -0,0 +1,21 @@
+import { HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService {
+
+  constructor() { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem('token');
+      if(token){
+        const authReq = req.clone({
+          headers: req.headers.set('Authorization', `Bearer ${token}`),
+        });
+        return next.handle(authReq);
+      }
+      return next.handle(req);
+  }
+}
---
+++ UI/src/app/services/guard-services/login-guard/login.guard.ts
@@ -0,0 +1,13 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../auth-service/auth.service';
+
+export const loginGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const auth = inject(AuthService);
+  if(auth.isAuthenticated()){
+    router.navigate(['/shared']);
+    return false;
+  }
+return true;
+};
