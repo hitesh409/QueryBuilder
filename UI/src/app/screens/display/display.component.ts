081cf40cafb50195688f71ea977baa6cb01e67d1
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2024-05-06 16:31:14 +0530

    modified layout of screens

--- UI/src/app/screens/display/display.component.ts
+++ UI/src/app/screens/display/display.component.ts
@@ -1,3 +1,4 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -5,6 +6,17 @@ import { Router } from '@angular/router';
   selector: 'app-display',
   templateUrl: './display.component.html',
   styleUrl: './display.component.css',
+  animations: [
+    trigger('fadeInOut', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('void <=> *', animate('0.4s ease')),
+    ]),
+  ],
 })
 export class DisplayComponent {
   isExpanded = false;
